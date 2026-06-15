bc179dedfa307a7c27a174142efa3b56d3c70460
NAVANEETH V SANKAR <navaneethvsankar@gmail.com>
2026-06-15 15:58:58 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 0;
+        int r = x/2+1;
+        int ans = 0;
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            long sq = (long)mid * mid;
+            if(sq == x)
+            {
+                return mid;
+            }
+            else if(sq<x)
+            {
+                ans = mid;
+                l = mid+1;
+            }
+            else 
+            {
+                r = mid-1;
+            }
+        }
+        return ans;        
+    }
+}
