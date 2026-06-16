19632ed315360e15eda7e65041c307565eb4cb71
NAVANEETH V SANKAR <navaneethvsankar@gmail.com>
2026-06-16 16:26:21 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    
+    public boolean isPalindrome(String s, int l, int r)
+    {
+
+            while(l<=r)
+            {
+                if(s.charAt(l) != s.charAt(r))
+                {
+                    return false;
+                }
+                else
+                {
+                    l++;
+                    r--;
+                }
+            }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length()-1;
+        int k = 0;
+            while(l<=r)
+            {
+                if(s.charAt(l) == s.charAt(r))
+                {
+                    l++;
+                    r--;
+                }
+                else
+                {
+                    if(isPalindrome(s, l, r-1) || isPalindrome(s, l+1, r) )
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }   
+        return true;
+    }
+}
